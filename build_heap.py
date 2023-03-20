e806e930b82c0b169a992d19b5b3740cdd9d751e
NatalijaValenika <125027023+NatalijaValenika@users.noreply.github.com>
2023-03-20 04:23:11 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,25 @@
 # python3
-
+import os
 
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    abc=lens(data) 
+    for i in range (abc//2,-1,-1):
+    def maz(data, i, swaps):
+    abc = len(data)
+    min_inde = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < abc and data[left] < data[min_inde]:
+        min_inde = left
+    if right < abc and data[right] < data[min_inde]:
+        min_inde = right
+    if min_inde != i:
+        swaps.append((i, min_inde))
+        data[i], data[min_inde] = data[min_inde], data[i]
+        maz(data, min_inde, swaps) 
     return swaps
 
 
@@ -16,6 +29,8 @@ def main():
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
+    inp = input()
+    if "I" in inp:
 
     # input from keyboard
     n = int(input())
@@ -23,14 +38,17 @@ def main():
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
+    
+    elif "F" in inp
+    file = input()
+        find = './tests/'
+        file2 = os.path.join(find, file)
+        with open(file2, mode="r") as file:
+         n = int(file.readline())
+            data = list(map(int, file.readline().split()))
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
+    
 
     # output all swaps
     print(len(swaps))
